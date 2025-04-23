80e82f522d2249c7d93f6babebbe8e19fb6ce76b
donquidofi612 <168350352+donquidofi612@users.noreply.github.com>
2025-04-23 22:15:24 +0000

    Update config.js

--- config.js
+++ config.js
@@ -17,66 +17,66 @@ export default {
       messagingSenderId: '...',
       appId: process.env.FIREBASE_APPID
     },
-    client_name: ' Christophe Fortin',
-    client_fullname: 'Mr  Christophe Fortin',
+    client_name: 'MONGIS-COUTURIER VIVIANE',
+    client_fullname: 'Mr/Mme MONGIS-COUTURIER VIVIANE',
     // frais_deblocage: '39.880€',
-    solde: '4876078',
+    solde: '12000',
     currency: '€',
     operations: [
         {
-            message: "transfert sepa reçu de EDF actionnaires",
-            date: "01/02/1995",
-            amount: "1020€",
+            message: "Virement ",
+            date: "23/04/2025",
+            amount: "12000€",
             type: "out"
         },
-        {
-            message: "Virement reçu de AREVA groupe ",
-            date: "07/02/1995",
-            amount: "279.000€",
-            type: "in"
-        },
-        {
-            message: "Virement pension de retraite de caisse de sécurité sociale",
-            date: "20/03/1995",
-            amount: "8.425€",
-            type: "in"
-        },
-        {
-            message: "Virement recu de MONABANQ",
-            date: "20/07/1995",
-            amount: "590€",
-            type: "in"
-        },
-        {
-            message: "Virement sepa émis à Claudine SAUSSET",
-            date: "20/04/1996",
-            amount: "1000€",
-            type: "out"
-        },
-        {
-            message: "Virement reçu de Sonia Bertrand",
-            date: "20/05/1996",
-            amount: "4800€",
-            type: "in"
-        },
-        {
-            message: "Virement reçu de Antoine McCANN",
-            date: "19/10/1996",
-            amount: "3500€",
-            type: "in"
-        },
-        {
-            message: "Dividende reçu de BBVA",
-            date: "20/11/1995",
-            amount: "10.000€",
-            type: "in"
-        },
-        {
-            message: "Dépôt initial de Nathalie provost",
-            date: "19/07/1997",
-            amount: "30000€",
-            type: "in"
-        },
+    //    {
+    //        message: "Virement reçu de AREVA groupe ",
+     //       date: "07/02/1995",
+     //       amount: "279.000€",
+     //       type: "in"
+     //   },
+     //   {
+      //      message: "Virement pension de retraite de caisse de sécurité sociale",
+      //      date: "20/03/1995",
+      //      amount: "8.425€",
+      //      type: "in"
+     //   },
+      //  {
+      //      message: "Virement recu de MONABANQ",
+      //      date: "20/07/1995",
+      //      amount: "590€",
+      //      type: "in"
+     //   },
+    //    {
+      //      message: "Virement sepa émis à Claudine SAUSSET",
+      //      date: "20/04/1996",
+      //      amount: "1000€",
+      //      type: "out"
+     //   },
+    //    {
+     //       message: "Virement reçu de Sonia Bertrand",
+      //      date: "20/05/1996",
+       //     amount: "4800€",
+       //     type: "in"
+       // },
+       // {
+          //  message: "Virement reçu de Antoine McCANN",
+          //  date: "19/10/1996",
+          //  amount: "3500€",
+          //  type: "in"
+        //},
+      //  {
+           // message: "Dividende reçu de BBVA",
+           // date: "20/11/1995",
+         //   amount: "10.000€",
+       //     type: "in"
+     //   },
+   //     {
+  //          message: "Dépôt initial de Nathalie provost",
+  //          date: "19/07/1997",
+  //          amount: "30000€",
+ //          type: "in"
+ //       },
     ],
     //Gestionnaire_name: 'Hamza Boutaleb',
     // client_fullname: 'Mme Aline Courbor Thiemonge',
